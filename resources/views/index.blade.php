704759cc72089b1d65cf7a8ef9122aa5369d876d
Md-Bari <bari15-5598@diu.edu.bd>
2025-07-03 21:24:59 +0600

    register updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   @stack('style')
+  @stack('style')
   <title>Tour Freak</title>
   <link rel="stylesheet" href="style.css">
 
