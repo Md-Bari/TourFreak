74ffec75a9532e59a7557879c1a2d1bb97b68610
Md-Bari <bari15-5598@diu.edu.bd>
2025-07-29 23:26:12 +0600

    admin nav added

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -10,12 +10,24 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('admin.home') }}">Admin Dashboard</a>
-        <div class="collapse navbar-collapse">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminNavbar" aria-controls="adminNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="adminNavbar">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <form method="POST" action="{{ route('logout') }}">
+                    <a class="nav-link" href="{{ route('admin.packages') }}">Update Package</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('room') }}">Update Room</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Available</a>
+                </li>
+                <li class="nav-item">
+                    <form method="POST" action="{{ route('logout') }}" class="d-inline">
                         @csrf
-                        <button type="submit" class="btn btn-sm btn-outline-light">Logout</button>
+                        <button type="submit" class="btn btn-sm btn-outline-light ms-2">Logout</button>
                     </form>
                 </li>
             </ul>
@@ -27,6 +39,11 @@
     @yield('content')
 </div>
 
+<footer class="bg-dark text-light text-center py-3 mt-5">
+    <div class="container">
+        <small>&copy; {{ date('Y') }} TourFreak Admin Panel. All rights reserved.</small>
+    </div>
+</footer>
+
 @stack('script')
-</body>
-</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap
--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,10 @@ Route::post('/contact', [ContactController::class, 'submit'])->name('contact.sub
 // -------------------- admin --------------------
 Route::get('/admin/home', [HomeController::class, 'index'])->name('admin.home');
 Route::get('/admin/packages', [TourPackageController::class, 'index'])->name('admin.packages');
+Route::post('/admin/packages/store', [TourPackageController::class, 'store'])->name('admin.packages.store');
+Route::get('/admin/packages/edit/{id}', [TourPackageController::class, 'edit'])->name('admin.packages.edit');
+Route::delete('/admin/packages/delete/{id}', [TourPackageController::class, 'destroy'])->name('admin.packages.delete');
+Route::put('/admin/packages/update/{id}', [TourPackageController::class, 'update'])->name('admin.packages.update');
+use App\Http\Controllers\RoomController;
+
+Route::get('/room-details/{type}', [RoomController::class, 'show'])->name('room.details');
